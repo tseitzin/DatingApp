d4f0f8b423f35bf200f794dba6809d2b64e58885
tseitzin <tseitzinger@gmail.com>
2024-06-24 15:53:44 -0400

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -15,6 +15,8 @@ builder.Services.AddDbContext<DataContext>(opt =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,6 +29,9 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().
+    WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
