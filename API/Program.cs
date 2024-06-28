65a1eb5d587d024fb846d1f43a011163898d16f3
tseitzin <tseitzinger@gmail.com>
2024-06-28 13:45:50 -0400

    Cleaned up using statements

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,4 @@
-using System.Text;
-using API;
-using API.Data;
 using API.Extensions;
-using API.Interfaces;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
