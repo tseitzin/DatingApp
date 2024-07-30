9ca0b206f5eba277175cf28e18b47c9cee7a4416
tseitzin <tseitzinger@gmail.com>
2024-07-29 21:25:21 -0400

    End of section 14

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -11,5 +11,25 @@ namespace API.Data
     public class DataContext(DbContextOptions options) : DbContext(options)
     {
         public DbSet<AppUser> Users { get; set; }
+        public DbSet<UserLike> Likes { get; set; }
+
+        override protected void OnModelCreating(ModelBuilder builder){
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserLike>()
+                .HasKey(k => new {k.SourceUserId, k.TargetUserId});
+            
+            builder.Entity<UserLike>()
+                .HasOne(s => s.SourceUser)
+                .WithMany(l => l.LikedUsers)
+                .HasForeignKey(s => s.SourceUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<UserLike>()
+                .HasOne(s => s.TargetUser)
+                .WithMany(l => l.LikedByUsers)
+                .HasForeignKey(s => s.TargetUserId)
+                .OnDelete(DeleteBehavior.Cascade); //SQL Server does not like cascade for both, use noaction instead
+        }
     }
 }
---
+++ API/Interfaces/ILikesRepository.cs
@@ -0,0 +1,17 @@
+﻿using API.DTOs;
+using API.Entities;
+using API.Helpers;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace API.Interfaces;
+
+public interface ILikesRepository
+{
+    Task<UserLike?> GetUserLike(int sourceId, int targetUserId);
+    Task <PagedList<MemberDto>> GetUserLikes(LikesParams likesParams);
+    Task<IEnumerable<int>> GetCurrentUserLikeIds(int currentUserId);
+    void DeleteLike(UserLike like);
+    void AddLike(UserLike like);
+    Task<bool> SaveChanges();
+
+}
